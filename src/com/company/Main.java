fee51e15028d81469f9d8fccb48e62cb5c1d1585
Davit Margaryan <dmargaryan88@gmail.com>
2022-12-02 19:27:39 +0400

    upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Product> productList = createBasicList();
+        List<Product> bookList = productList.stream().filter(p -> p.getCategory().equals(Category.Book)).collect(Collectors.toList());
+        System.out.println(bookList);
+        List<Product> babyList = productList.stream().filter(p -> p.getCategory().equals(Category.Baby)).collect(Collectors.toList());
+        System.out.println(babyList);
+
+        System.out.println("Summ = " + bookList.stream().mapToDouble(Product::getPrice).sum());
+        System.out.println("Average = " + bookList.stream().mapToDouble(Product::getPrice).average());
+        System.out.println("Max = " + bookList.stream().mapToDouble(Product::getPrice).max());
+        System.out.println("Min = " + bookList.stream().mapToDouble(Product::getPrice).min());
+        System.out.println("Count = " + bookList.stream().mapToDouble(Product::getPrice).count());
+
+    }
+
+    public static List createBasicList() {
+        Product product1 = new Product(100, "A", Category.Baby);
+        Product product2 = new Product(110, "B", Category.Baby);
+        Product product3 = new Product(10, "C", Category.Book);
+        Product product4 = new Product(20, "D", Category.Book);
+        Product product5 = new Product(30, "E", Category.Baby);
+        Product product6 = new Product(40, "F", Category.Book);
+
+        List<Product> productList = new LinkedList<>();
+        productList.add(product1);
+        productList.add(product2);
+        productList.add(product3);
+        productList.add(product4);
+        productList.add(product5);
+        productList.add(product6);
+
+        return productList;
+    }
+}
